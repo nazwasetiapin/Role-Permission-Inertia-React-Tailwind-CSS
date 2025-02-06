9083bb5df77625f3502187acfc1ace085a3d6ef7
nazwastpn <nazwasetiapin@gmail.com>
2025-02-06 09:03:29 +0700

    Permissions

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -4,14 +4,21 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class PermissionSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        $createPost = Permission::create(['name' => 'create_post']);
+        $editPost = Permission::create(['name' => 'edit_post']);
+        $deletePost = Permission::create(['name' => 'delete_post']);
+
+        $role = Role::create(['name' => 'editor']);
+        $role->givePermissionTo($createPost, $editPost, $deletePost);
     }
 }
